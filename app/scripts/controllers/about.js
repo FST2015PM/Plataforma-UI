a444594afad7e0aecac1f3643aa4154b3d446250
Hasdai Pacheco <haxdai@gmail.com>
2017-04-24 20:38:17 -0500

    Added yeoman boilerplate

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name plataformaUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the plataformaUiApp
+ */
+angular.module('plataformaUiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
